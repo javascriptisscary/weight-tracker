0749dbf820f49f20808bbbc66becdd93cc5dfc24
Kenneth <crazedpokerpro@yahoo.com>
2015-12-02 15:58:00 -0500

    fix css for heroku

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -141,11 +141,12 @@ class UsersController < ApplicationController
     end
   end
 
+  
+  
+  
+  
   private
     # Use callbacks to share common setup or constraints between actions.
-  
-    
-    
     
     def set_user
       @user = User.find(params[:id])
